844a495392f938d8ebdc09980286e6ad469450d9
Yangbin Wei <weiyangbin0812@126.com>
2020-02-10 19:54:17 +0800

    Update and rename 删除排列数组中的重复项.md to 删除排列数组中的重复项.py

--- 删除排列数组中的重复项.py
+++ 删除排列数组中的重复项.py
@@ -1,5 +1,6 @@
+“”“
 原地算法， 定义两个指针，如果两数字不同，进行保存
-```
+”“”
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         if len(nums) == 0: return 0  
@@ -12,4 +13,14 @@ class Solution:
             
         return i + 1
                 
-```
+“”“
+利用排序特性遍历删除
+”“”
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pointer = 1
+        while pointer < len(nums):
+            if nums[pointer - 1] == nums[pointer]:
+                del nums[pointer]
+            else:
+                pointer += 1
