69df129bfabf22bbbfa2689ab9f37998c63e277d
Yangbin Wei <weiyangbin0812@126.com>
2020-03-26 20:10:29 +0800

    Create 合并两个有序数组.cpp

---
+++ 合并两个有序数组.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len = 0;
+        for(int i=m; i < nums1.size(); i++)
+        {
+            nums1[i] = nums2[len];
+            len++;
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
